26b6c1e1cfead682bde6b0dcb1d8bc8fbd5e8f6d
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-02-24 16:03:46 -0300

    arreglo

--- app/models/product.rb
+++ app/models/product.rb
@@ -21,6 +21,5 @@ class Product < ApplicationRecord
   has_attached_file :photo2, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :photo2, content_type: /\Aimage\/.*\z/
 
-  mount_uploader :image, ImageUploader
 
 end
