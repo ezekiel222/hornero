2879245960ebac21517ba050b1a2691bfe0e5518
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-02-26 13:50:11 -0300

    arreg

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -1,6 +1,5 @@
-gateway = Braintree::Gateway.new(
-  :environment => :sandbox,
-  :merchant_id => ENV.fetch('BRAINTREE_MERCHANT_ID'),
-  :public_key => ENV.fetch('BRAINTREE_PUBLIC_KEY'),
-  :private_key => ENV.fetch('BRAINTREE_PRIVATE_KEY'),
-)
+Braintree::Configuration.environment = :sandbox
+ Braintree::Configuration.logger = Logger.new('log/braintree.log')
+ Braintree::Configuration.merchant_id = ENV['BRAINTREE_MERCHANT_ID']
+ Braintree::Configuration.public_key = ENV['BRAINTREE_PUBLIC_KEY']
+ Braintree::Configuration.private_key = ENV['BRAINTREE_PRIVATE_KEY']
