cea35eb89e36bc98c9bc1bd57100886d573ead95
Ezekiel Grosfeld <ezekielgrosfeld@pop-os.localdomain>
2019-03-08 12:14:50 -0300

    findecompra

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "products#index"
   get 'home/favoritos'
+  get 'home/compra'
 end
